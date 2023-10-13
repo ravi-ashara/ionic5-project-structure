bcaaca88e697452e35074d6346e677ad90537638
Ravi <rashara@brightree.com>
2023-10-13 14:44:03 +0530

    Add push notification and biometric login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { PushNotifications } from '@capacitor/push-notifications';
+import { Platform } from '@ionic/angular';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,7 +10,7 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {
+  constructor(private platform: Platform) {
     if (localStorage.theme) {
       if (localStorage.theme == 'dark')
         document.body.classList.add('dark');
@@ -14,5 +18,60 @@ export class AppComponent {
     } else {
       document.body.setAttribute('data-theme', 'light');
     }
+
+    this.nativePushNotificationEnable();
+  }
+
+  async pushNotificationInitialize() {
+    PushNotifications.register();
+  }
+
+  async checkPermission() {
+    // Request permission to use push notifications
+    // iOS will prompt user and return if they granted permission or not
+    // Android will just grant without prompting for version below 12, for 13+,checkPermissions added.
+    if (this.platform.is('ios')) {
+      this.requestPermission();
+      return;
+    }
+    PushNotifications.checkPermissions().then((permission) => {
+      if (permission.receive === 'prompt' || permission.receive === 'prompt-with-rationale') {
+        this.requestPermission();
+      } else if (permission.receive === 'granted') {
+        this.pushNotificationInitialize();
+      }
+    });
+  }
+  requestPermission() {
+    PushNotifications.requestPermissions().then((result) => {
+      if (result.receive === 'granted') {
+        this.pushNotificationInitialize();
+      } else {
+        // Show some error
+      }
+    });
+  }
+
+  async nativePushNotificationEnable() {
+    // TODO: CHP-3664
+    PushNotifications.removeAllListeners();
+    this.checkPermission();
+
+    await PushNotifications.addListener('registration', token => {
+      localStorage.setItem('pushToken', token.value);
+      console.info('Registration token: ', token);
+    });
+
+    await PushNotifications.addListener('registrationError', err => {
+      console.error('Registration error: ', err.error);
+    });
+
+    await PushNotifications.addListener('pushNotificationReceived', notification => {
+      console.log('Push notification received: ', notification);
+    });
+
+    await PushNotifications.addListener('pushNotificationActionPerformed', notification => {
+      console.log('Push notification action performed', notification.actionId, notification.inputValue);
+    });
   }
 }
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,7 @@
 import { apiMethod } from 'src/app/constants/constants';
 import { CallApiService } from './../../services/callApi/call-api.service';
 import { Component, OnInit } from '@angular/core';
+import { BiometryType, NativeBiometric } from "capacitor-native-biometric";
 
 @Component({
   selector: 'app-login',
@@ -12,6 +13,45 @@ export class LoginPage implements OnInit {
   constructor(public callAPI: CallApiService) { }
 
   ngOnInit() {
+    this.setCredentials();
   }
 
+
+  async getCredential() {
+    const credentials = await NativeBiometric.getCredentials({
+      server: "the-example-demo",
+    });
+    console.log('credentials', credentials);
+  }
+
+  async performBiometricVerification() {
+    const result = await NativeBiometric.isAvailable();
+    const touchIdKey = localStorage.getItem('touchIdKey');
+
+    if (!result.isAvailable && !touchIdKey) return;
+
+    const verified = await NativeBiometric.verifyIdentity({
+      reason: "For easy log in",
+      title: "Log in",
+      description: "Maybe a description too?",
+    })
+      .then(() => true)
+      .catch(() => false);
+
+    if (!verified) return;
+
+    this.getCredential();
+  }
+
+  async setCredentials() {
+    const result = await NativeBiometric.isAvailable();
+    if (!result.isAvailable) return
+    // Save user's credentials
+    await NativeBiometric.setCredentials({
+      username: "ravi",
+      password: "Passw0rd",
+      server: "the-example-demo",
+    });
+    localStorage.setItem('touchIdKey', 'ravi');
+  }
 }
